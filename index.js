892a83f0887ae1350a1583c4e309e2e463926a17
Akash Akter <AkashAkter>
2022-10-25 13:19:33 +0600

    vercel set up done

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
+
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+const courses = require('./course-details.json')
+
+app.get('/', (req, res) => {
+    res.send('API in running');
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.listen(port, () => console.log('running in', port))
